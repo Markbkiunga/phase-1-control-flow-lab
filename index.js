492621e1b0679af71a265618392c9778510db1d8
Mark Brian <markbkiunga2017@gmail.com>
2024-06-25 19:55:42 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceCovered) {
+  if (distanceCovered <= 400) {
+    console.log("This one is on me!");
+    return "This one is on me!";
+  } else if (distanceCovered <= 2000) {
+    console.log("That will be twenty bucks.");
+    return "That will be twenty bucks.";
+  } else if (distanceCovered <= 2500) {
+    console.log("I will gladly take your thirty bucks.");
+    return "I will gladly take your thirty bucks.";
+  } else {
+    console.log("No can do.");
+    return "No can do.";
+  }
 }
+scuberGreetingForFeet(2501);
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  city === "NYC" ? "Ok, sounds good." : "No go.";
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log(ternaryCheckCity("NYC"));
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      console.log("Thank you so much.");
+      return "Thank you so much.";
+    case "not as generous":
+      console.log("Thank you.");
+      return "Thank you.";
+    default:
+      console.log("Bye.");
+      return "Bye.";
+  }
 }
+switchOnCharmFromTip("not as generous");
